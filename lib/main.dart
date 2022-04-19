0af0c6d3f0af1c319e0f0377c5aa1de63e77e3de
Vedat Biner <vbiner@gmail.com>
2022-04-19 17:12:23 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+// Interaktif deneyim örneği
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  _MyHomePageState() {
+    print("my home page yaratılıyor.");
+  }
+
+  int aktifButon = 0;
+
+  @override
+  void didUpdateWidget(MyHomePage oldWidget) {
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              // aktif buton balısılabilir halde ise 0, delise null olsun.
+                onPressed: aktifButon == 0 ? (){
+                  print("0");
+                  setState(() {
+                    aktifButon = (aktifButon + 1) % 2;
+                  });
+                } : null,
+                child: Text("0"),
+            ),
+            ElevatedButton(
+              onPressed: aktifButon == 1 ? (){
+                print("1");
+                setState(() {
+                  aktifButon = (aktifButon + 1) % 2;
+                });
+              } : null,
+              child: Text("1"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
